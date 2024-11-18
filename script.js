4c257666fb876106800cdcb444979a84ff998104
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-11-17 19:46:13 -0500

    Update game to use buttons, and play to first player with 5 points

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const choice = document.getElementsByName('choice');
-const rounds = document.getElementById('rounds');
-const result = document.getElementById('result');
-const submitButton = document.getElementById('submit');
-const score = document.getElementById('score');
+const rounds = document.querySelector('#rounds');
+const result = document.querySelector('#result');
+const choices = document.querySelector('.choices');
+const reset = document.querySelector('#reset')
+const score = document.querySelector('#score');
 
-
+let humanScore = 0;
+let computerScore = 0;
+score.innerHTML =  `Human: <strong>${humanScore}</strong> | Computer: <strong>${computerScore}</strong>`;
+reset.style.display = 'none';
 
 // 1. Get user choice
 //     - Use radio buttons to chooose
@@ -17,16 +20,24 @@ const score = document.getElementById('score');
 //     - paper beats rock
 // 4. Print winner to the screen
 
-function getHumanChoice() {
-    while (true) {
-        let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+reset.addEventListener('click', resetGame);
 
-        if (humanChoice === 'rock' || humanChoice === 'scissors' || humanChoice === 'paper') {
-            return humanChoice;
-        }
-        alert('Please enter a valid choice.');
+choices.addEventListener('click', (event) => {
+    let target = event.target;
+    const computerChoice = getComputerChoice();
+
+    switch(target.id) {
+        case 'rock':
+            playRound('rock', computerChoice);
+            break;
+        case 'paper':
+            playRound('paper', computerChoice);
+            break;
+        case 'scissors':
+            playRound('scissors', computerChoice);
+            break;
     }
-}
+})
 
 
 function getComputerChoice() {
@@ -35,44 +46,52 @@ function getComputerChoice() {
     return options[randNum];
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let currentRound = 1;
+function playRound(humanChoice, computerChoice) {
+    const para = document.createElement('p');
 
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'scissors' && computerChoice === 'paper' || humanChoice === 'paper' && computerChoice === 'rock') {
-            rounds.innerHTML += `Player chose <strong>${humanChoice}</strong>, computer chose <strong>${computerChoice}</strong>. <strong>${humanChoice}</strong> beats <strong>${computerChoice}</strong>. Player wins!`;
-            humanScore++;
-        } else if (humanChoice === 'rock' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'paper' ) {
-            rounds.innerHTML += `Player chose <strong>${humanChoice}</strong>, computer chose <strong>${computerChoice}</strong>. <strong>${humanChoice}</strong> ties <strong>${computerChoice}</strong>. It's a tie!`;
-        } else {
-            rounds.innerHTML += `Player chose <strong>${humanChoice}</strong>, computer chose <strong>${computerChoice}</strong>. <strong>${computerChoice}</strong> beats <strong>${humanChoice}</strong>. Computer wins!`;
-            computerScore++;
-        }
+    if (humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'scissors' && computerChoice === 'paper' || humanChoice === 'paper' && computerChoice === 'rock') {
+        para.innerHTML += `Player chose <strong>${humanChoice}</strong>, computer chose <strong>${computerChoice}</strong>. <strong>${humanChoice}</strong> beats <strong>${computerChoice}</strong>. Player wins!`;
+        rounds.appendChild(para);
+        humanScore++;
+        score.innerHTML =  `Human: <strong>${humanScore}</strong> | Computer: <strong>${computerScore}</strong>`;
+        checkForWinner();
+    } else if (humanChoice === 'rock' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'paper' ) {
+        para.innerHTML += `Player chose <strong>${humanChoice}</strong>, computer chose <strong>${computerChoice}</strong>. <strong>${humanChoice}</strong> ties <strong>${computerChoice}</strong>. It's a tie!`;
+        rounds.appendChild(para);
+        score.innerHTML =  `Human: <strong>${humanScore}</strong> | Computer: <strong>${computerScore}</strong>`;
+        checkForWinner();
+    } else {
+        para.innerHTML += `Player chose <strong>${humanChoice}</strong>, computer chose <strong>${computerChoice}</strong>. <strong>${computerChoice}</strong> beats <strong>${humanChoice}</strong>. Computer wins!`;
+        rounds.appendChild(para);
+        computerScore++;
+        score.innerHTML =  `Human: <strong>${humanScore}</strong> | Computer: <strong>${computerScore}</strong>`;
+        checkForWinner();
     }
-    function playNextRound() {
-        if (currentRound <= 5) {
-            const humanSelection = getHumanChoice();
-            const computerSelection = getComputerChoice();
-            rounds.innerHTML += `<h2>Round ${currentRound}</h2>`;
-            playRound(humanSelection, computerSelection);
-            currentRound++;
-            setTimeout(playNextRound, 100);   
-        } else {
-            result.innerHTML += `<h2> Results</h2>`;
-            if (humanScore > computerScore) {
-                result.innerHTML += `Human beats computer with a final score of: Human: <strong>${humanScore}</strong> | Computer: <strong>${computerScore}</strong>`;
-            } else if (humanScore < computerScore) {
-                result.innerHTML += `Computer beats human with a final score of: Human: <strong>${humanScore}</strong> | Computer: <strong>${computerScore}</strong>`;
-            } else {
-                result.innerHTML += `It's a tie with a final score of: Human: <strong>${humanScore}</strong> | Computer: <strong>${computerScore}</strong>`;
-            }
-        }
-    }
-
-    playNextRound();
-
 }
 
-playGame();
+
+function checkForWinner() {
+    if (humanScore === 5 || computerScore === 5) {
+        if (humanScore === 5) {
+            result.innerHTML += `Human beats computer with a final score of: Human: <strong>${humanScore}</strong> | Computer: <strong>${computerScore}</strong>`;
+        } else {
+            result.innerHTML += `Computer beats human with a final score of: Human: <strong>${humanScore}</strong> | Computer: <strong>${computerScore}</strong>`;
+        }
+        document.querySelector('#rock').disabled = true;
+        document.querySelector('#paper').disabled = true;
+        document.querySelector('#scissors').disabled = true;
+        reset.style.display = 'block';
+    }
+};
+
+function resetGame() {
+    result.innerHTML = '';
+    rounds.innerHTML = '';
+    humanScore = 0;
+    computerScore = 0;
+    score.innerHTML = score.innerHTML =  `Human: <strong>${humanScore}</strong> | Computer: <strong>${computerScore}</strong>`;
+    document.querySelector('#rock').disabled = false;
+    document.querySelector('#paper').disabled = false;
+    document.querySelector('#scissors').disabled = false;
+    reset.style.display = 'none';
+}
